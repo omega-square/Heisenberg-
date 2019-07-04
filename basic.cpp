98efdb57044e7d8a77256074e8f470291b7422a0
omega-square <45560738+omega-square@users.noreply.github.com>
2019-07-04 20:56:21 +0530

    Add files via upload
    non - test case basic

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f(a,b,c) for(int a=b;a<c;a++)
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.tie(NULL);
+
+
+}
+
