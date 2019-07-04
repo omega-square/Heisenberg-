51be287500b5a4aa2d6e1b1e0c1ebb3b2b0d1506
omega-square <45560738+omega-square@users.noreply.github.com>
2019-07-04 21:15:09 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 #define ll long long
 #define f(a,b,c) for(int a=b;a<c;a++)
+#define num(x) (int)(x-'0')
+#define charnum(x) (int)(x-'a')
 int main()
 {
 	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+        cin.tie(NULL);
 	cout.tie(NULL);
+	
 
 
 }
